b35503857a1de8e1974d04e2b1ae84442f1a254f
Onkar Deshpande <62983112+onkar964@users.noreply.github.com>
2025-06-18 11:47:27 +0530

    Update ProductDisplay.jsx

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -11,17 +11,17 @@ const ProductDisplay = (props) => {
     <div className='productdisplay'>
         <div className="productdisplay-left">
             <div className="productdisplay-img-list">
-                <img src={product.image} alt="loading" />
-                <img src={product.image} alt="loading" />
-                <img src={product.image} alt="loading" />
-                <img src={product.image} alt="loading" />
+                <img src={product?.image} alt="loading" />
+                <img src={product?.image} alt="loading" />
+                <img src={product?.image} alt="loading" />
+                <img src={product?.image} alt="loading" />
             </div>
             <div className="productdisplay-img">
-                <img className='prodctdisplay-main-img' src={product.image} alt="loading" />
+                <img className='prodctdisplay-main-img' src={product?.image} alt="loading" />
             </div>
         </div>
         <div className="productdisplay-right">
-            <h1>{product.name}</h1>
+            <h1>{product?.name}</h1>
             <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -31,8 +31,8 @@ const ProductDisplay = (props) => {
                 <p>(122)</p>
             </div>
             <div className="productdisplay-right-prices">
-                <div className="productdisplay-right-price-old">₹ {product.old_price}</div>
-                <div className="productdisplay-right-price-new">₹ {product.new_price}</div>
+                <div className="productdisplay-right-price-old">₹ {product?.old_price}</div>
+                <div className="productdisplay-right-price-new">₹ {product?.new_price}</div>
             </div>
             <div className="productdisplay-right-description">Lorem ipsum dolor sit elit. Quis architecto, corporis, officiis et aliquid dolorum asperiores quam exercitationem nulla dolorem saepe deleniti dicta minus. Veritatis reiciendis repellendus porro animi excepturi!</div>
             <div className="productdisplay-right-size">
